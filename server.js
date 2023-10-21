772cdbaec8aa389b236da3088cec84f2a14827b1
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2023-10-21 18:28:20 +0530

    set up of json middleware

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data recieved" });
 });
 
 app.listen(5100, () => {
