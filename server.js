6394875167e2fd55ea382019ac56c50aabdde38f
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2023-10-21 18:36:00 +0530

    morgan and dotenv setup

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-
+import morgan from "morgan";
 const app = express();
 
-app.use(express.json());
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data recieved" });
 });
 
-app.listen(5100, () => {
-  console.log("server is running");
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log("server is running...");
 });
