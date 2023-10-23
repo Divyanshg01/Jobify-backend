a84ec7848512d6aa8c621be9a3355d646f228ba7
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2023-10-23 23:20:52 +0530

    added validation middleware

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import express from "express";
 import morgan from "morgan";
 import mongoose from "mongoose";
 //CUSTOM IMPORTS
-
+import { validateTest } from "./middleware/validationMiddleware.js";
 //routers
 import jobRouter from "./routes/jobRouter.js";
 
@@ -18,6 +18,10 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+app.post("/api/v1/test", validateTest, (req, res) => {
+  const { name } = req.body;
+  res.json({ message: `hello ${name}` });
+});
 
 app.use(express.json());
 app.use("/api/v1/jobs", jobRouter);
@@ -26,7 +30,6 @@ app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
 });
 app.use(errorHandlerMiddleware);
-
 
 const port = process.env.PORT || 5100;
 
